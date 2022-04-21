44877a65721e67145bbcf9fccb60c9e9566a7cf1
Josh Peake <joshpeake@live.com>
2022-04-21 22:09:53 +0100

    updated experiments

--- ACOCode/noveltyExperiments.py
+++ ACOCode/noveltyExperiments.py
@@ -5,24 +5,26 @@ import csv
 from unittest import result
 
 ant_commands = ["antmain.exe", "pcb442.tsp","500","16","40","99999", "20"]
-num_seeds = 2
+num_seeds = 5
 trials_per_seed = 1
 num_tsp = 1
 seeds = []
-tsp = ["pcb442.tsp"]
-stagVals = [10,20,30,50,100,200,500]
+tsp = ["pcb442.tsp", "rat783.tsp", "pr1002.tsp"]
+stagVals = [10,20,30,50,100,200,500,1000,2000]
 
 for i in range(num_seeds):
     seeds.append(str(random.randint(0,99999)))
 
-with open('noveltytests.csv', 'w') as csvfile:
-    resultWriter = csv.writer(csvfile, delimiter=',',
+with open('noveltytests2.csv', 'w') as csvfile:
+    resultWriter = csv.writer(csvfile,
                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
     for i in seeds:
         for j in stagVals:
-            resultWriter.writerow(["Seed: " + str(i) + " Stag: " + str(j)])
-            ant_commands[5] = str(i)
-            ant_commands[6] = str(j)
-            output = subprocess.check_output(ant_commands)
-                
-            resultWriter.writerow(str(output))
+            for k in tsp:
+                resultWriter.writerow(["Seed: " + str(i) + " Stag: " + str(j) + " TSP: " + str(k)])
+                ant_commands[5] = str(i)
+                ant_commands[6] = str(j)
+                ant_commands[1] = str(k)
+                output = subprocess.check_output(ant_commands)
+                    
+                resultWriter.writerow(output.decode('utf-8').strip())
